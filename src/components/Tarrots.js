64f7d4f81874db5867ea56f3e738c5ebaadf1b5e
Paulina <pgalas40@gmail.com>
2023-03-24 14:53:57 +0100

    change bc & fix bug

--- src/components/Tarrots.js
+++ src/components/Tarrots.js
@@ -1,13 +1,6 @@
 const Tarrots = () => {
   let allTarrots = [
-    "Czym jest JavaScript?",
-    "W jaki sposób możemy deklarować zmienne w JavaScript?",
-    "Jakie typy danych są obsługiwane przez JavaScript?",
-    "Jaka jest różnica pomiędzy operatowami == oraz ===?",
-    "Czym jest DOM JavaScript?",
-    "Czym jest funkcja anonimowa?",
-    "Czym charakteryzuje się arrow function?",
-    "Czym są instrukcje warunkowe? Jakie znasz ich zastosowanie?",
+    
   ];
 
   return allTarrots;
